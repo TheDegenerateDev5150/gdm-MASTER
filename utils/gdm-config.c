19c544dd1ebda84d977fb4b2e8e7e690734c483b
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-07-19 12:03:56 +0200

    build: Install the gdm service files in the system pam config path
    Installing service files in /etc/pam.d it's not something that we should
    do since such path is meant to be used for configuration files and so
    potential sys admin overrides.
    
    As per this, install the service files in /usr/lib/pam.d by default and
    fallback to the /etc ones if none is found.
    
    Part-of: <https://gitlab.gnome.org/GNOME/gdm/-/merge_requests/380>

--- utils/gdm-config.c
+++ utils/gdm-config.c
@@ -1438,11 +1438,21 @@ static gboolean
 have_pam_module (GdmAuthType auth_type)
 {
         g_autofree char *pam_service = NULL;
+        g_autofree char *pam_service_name = NULL;
 
-        pam_service = g_strdup_printf (PAM_CONFIG_SERVICES_DIR "/gdm-%s",
-                auth_type_to_string (auth_type));
+        pam_service_name = g_strdup_printf ("/gdm-%s",
+                                            auth_type_to_string (auth_type));
+        pam_service = g_build_filename (PAM_CONFIG_SERVICES_DIR, pam_service_name, NULL);
 
-        g_debug ("Checking for PAM profile “%s” existance", pam_service);
+        g_debug ("Checking for PAM profile “%s” existence", pam_service);
+
+        if (g_file_test (pam_service, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_REGULAR)) {
+                return TRUE;
+        }
+
+        g_set_str (&pam_service, g_build_filename (PAM_SYS_SERVICES_DIR, pam_service_name, NULL));
+
+        g_debug ("Checking for PAM profile “%s” existence", pam_service);
 
         return g_file_test (pam_service, G_FILE_TEST_EXISTS | G_FILE_TEST_IS_REGULAR);
 }
